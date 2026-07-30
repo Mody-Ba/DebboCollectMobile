b9f958a7bc38b1abf0ad0703893f0fcc9766fe3c
Mody Ba <modyba@e2:1c:d2:49:4d:1a.home>
2026-07-30 01:54:11 -0400

    Fix URL API login vers Render au lieu du localhost

--- services/authService.ts
+++ services/authService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const API_URL = "http://192.168.2.70:8080/api/auth";
+const API_URL = "https://debbo-collect.onrender.com/api/auth";
 
 export const login = async (
     email: string,
